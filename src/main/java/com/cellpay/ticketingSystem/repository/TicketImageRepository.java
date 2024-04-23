b2d7a7b9fecc2f54c59f69e1f686ae7c0218cc2f
sagar-shrestha <sagarshrestha3111@gmail.com>
2024-04-23 11:01:47 +0545

    renamed all project to com.cellpay.ticketSystem to com.sagar.baseproject

--- src/main/java/com/cellpay/ticketingSystem/repository/TicketImageRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cellpay.ticketingSystem.repository;
-
-import com.cellpay.ticketingSystem.entity.TicketImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketImageRepository extends JpaRepository<TicketImage, Integer> {
-}
--- src/main/java/com/cellpay/ticketingSystem/repository/TicketTopicRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cellpay.ticketingSystem.repository;
-
-import com.cellpay.ticketingSystem.entity.TicketTopic;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TicketTopicRepository extends JpaRepository<TicketTopic, Integer> {
-}
